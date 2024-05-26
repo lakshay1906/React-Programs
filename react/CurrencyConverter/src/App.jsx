9b563cb91ef04f0a2a37b2a3f120775ba36c07e1
lakshay1906 <lbhati020@gmail.com>
2024-05-26 16:53:46 +0530

    Currency Converter

--- react/CurrencyConverter/src/App.jsx
+++ react/CurrencyConverter/src/App.jsx
@@ -1,37 +1,50 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import CurrencyConverter from "./Components/CurrencyConverter";
 
 function App() {
   console.log("Radhe Radhe");
   const [apiData, setApiData] = useState({});
+  const [currencies, setCurrencies] = useState({});
   const [load, setLoad] = useState(false);
+  const [finalLoad, setFinalLoad] = useState(false);
+  const [amount, setAmount] = useState(0);
   const [from, setFrom] = useState("AUD");
   const [to, setTo] = useState("USD");
-  const url = `https://currency-conversion-and-exchange-rates.p.rapidapi.com/latest?from=${from}&to=${to}%2CGBP`;
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "6d11a5a83emsh7c9ccac87e2ff7dp1505adjsn3e3cd76f48ff",
-      "X-RapidAPI-Host":
-        "currency-conversion-and-exchange-rates.p.rapidapi.com",
-    },
-  };
+  const currencyURL = `https://api.frankfurter.app/currencies`;
 
   async function fetchData() {
-    try {
-      const response = await fetch(url, options);
-      const result = await response.json();
-      console.log(result.rates);
-      setApiData(result.rates);
-      setLoad(true);
-    } catch (error) {
-      console.error(error);
+    if (amount > 0) {
+      if (from == to) {
+        alert("From and To are equal\nThis is not possible!!");
+        return;
+      }
+      const conversionURL = `https://api.frankfurter.app/latest?amount=${amount}&from=${from}&to=${to}`;
+      try {
+        const response = await fetch(conversionURL);
+        const result = await response.json();
+        console.log(result);
+        setApiData(result);
+        setFinalLoad(true);
+      } catch (error) {
+        console.error(error);
+      }
     }
   }
+  function fetchCurrencies() {
+    (async function () {
+      try {
+        const response = await fetch(currencyURL);
+        const result = await response.json();
+        console.log(result);
+        setCurrencies(result);
+        setLoad(true);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  useEffect(() => fetchCurrencies(), []);
 
   return (
     load && (
@@ -39,8 +52,15 @@ function App() {
         <div className="text-2xl w-full h-screen flex justify-center items-center bg-[#292929]">
           <CurrencyConverter
             data={apiData}
-            setFromFunc={{ from, setFrom }}
-            setToFunc={{ to, setTo }}
+            currenciesData={currencies}
+            fromValue={from}
+            setFromFunc={setFrom}
+            toValue={to}
+            setToFunc={setTo}
+            amountValue={amount}
+            setAmountFunc={setAmount}
+            conversionFunc={fetchData}
+            load={finalLoad}
           />
         </div>
       </>
--- react/CurrencyConverter/src/Components/CurrencyConverter.jsx
+++ react/CurrencyConverter/src/Components/CurrencyConverter.jsx
@@ -1,12 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { RiExchangeLine } from "react-icons/ri";
 
-function CurrencyConverter({ data, setFromFunc, setToFunc }) {
-  const [currencyOptions, setCurrencyOptions] = useState([]);
-  useEffect(() => {
-    setCurrencyOptions(Object.keys(data));
-    console.log(setFromFunc, setToFunc);
-  }, []);
+function CurrencyConverter({
+  data,
+  currenciesData,
+  fromValue,
+  setFromFunc,
+  toValue,
+  setToFunc,
+  amountValue,
+  setAmountFunc,
+  conversionFunc,
+  load,
+}) {
+  function onSwap() {
+    setFromFunc(toValue);
+    setToFunc(fromValue);
+  }
   return (
     <>
       <div className="bg-white w-7/12 min-h-[53%] text-xl text-black px-5 py-2 rounded-md flex flex-col items-center gap-y-3">
@@ -18,62 +28,77 @@ function CurrencyConverter({ data, setFromFunc, setToFunc }) {
               <select
                 id="from"
                 className="border border-black rounded-md"
-                onChange={(e) => setFromFunc.setFrom(e.target.value)}
-                value={setFromFunc.from}
+                onChange={(e) => setFromFunc(e.target.value)}
+                value={fromValue}
               >
-                {currencyOptions.sort().map((ele) => (
-                  <option value={ele} key={ele}>
-                    {ele}
-                  </option>
-                ))}
+                {Object.keys(currenciesData)
+                  .sort()
+                  .map((ele) => (
+                    <option value={ele} key={ele}>
+                      {ele}
+                    </option>
+                  ))}
               </select>
             </div>
-            <RiExchangeLine size="2.8rem" />
+            <RiExchangeLine size="2.8rem" onClick={() => onSwap()} />
             <div className="flex flex-col w-4/12">
               <label htmlFor="to">To</label>
               <select
                 id="to"
                 className="border border-black rounded-md"
-                onChange={(e) => setToFunc.setTo(e.target.value)}
-                value={setToFunc.to}
+                onChange={(e) => setToFunc(e.target.value)}
+                value={toValue}
               >
-                {currencyOptions.sort().map((ele) => (
-                  <option value={ele} key={ele}>
-                    {ele}
-                  </option>
-                ))}
+                {Object.keys(currenciesData)
+                  .sort()
+                  .map((ele) => (
+                    <option value={ele} key={ele}>
+                      {ele}
+                    </option>
+                  ))}
               </select>
             </div>
           </div>
-          <div className="flex flex-col w-full">
-            <label htmlFor="amount">Enter Amount:</label>
-            <input
-              id="amount"
-              type="number"
-              min={0}
-              className="border border-black px-2 py-1 rounded-md"
-              value={100000}
-            />
-          </div>
-          <div className="flex flex-col w-full">
-            <label htmlFor="calcAmount">Calculated Amount:</label>
-            <input
-              id="calcAmount"
-              type="number"
-              readOnly
-              min={0}
-              className="border border-black px-2 py-1 rounded-md"
-              value={100000}
-            />
-          </div>
+          <form
+            className="flex flex-col items-center gap-1"
+            onSubmit={(e) => e.preventDefault()}
+          >
+            <div className="flex flex-col w-full">
+              <label htmlFor="amount">Enter Amount:</label>
+              <input
+                id="amount"
+                type="number"
+                min={0}
+                className="border border-black px-2 py-1 rounded-md"
+                onChange={(e) => setAmountFunc(e.target.value)}
+                value={amountValue}
+                required
+              />
+            </div>
+            <div className="flex flex-col w-full">
+              <label htmlFor="calcAmount">Calculated Amount:</label>
+              <input
+                id="calcAmount"
+                type="number"
+                readOnly
+                min={0}
+                className="border border-black px-2 py-1 rounded-md"
+                value={load && data.rates[toValue] ? data.rates[toValue] : 0}
+              />
+            </div>
+            <button
+              className="bg-blue-700 w-fit text-white px-3 py-1 rounded-md mt-3 "
+              onMouseDown={() => conversionFunc()}
+            >
+              Convert
+            </button>
+          </form>
         </div>
-
-        <button className="bg-blue-700 w-fit text-white px-3 py-1 rounded-md mt-1">
-          Convert
-        </button>
       </div>
     </>
   );
 }
 
 export default CurrencyConverter;
+
+#React #ShoppingApp #WebDevelopment #Productivityspace #React #FolderOrganization #react #TechInnovation #realTimeData #memoization #reactJS #webDevelopment #javaScript #frontEnd #uiux #reactapp #codenewbie #techinnovation #programming #softwaredevelopment #webdev #developercommunity #digitaltransformation #techforgood #opensource #creativecoding #innovationhub #techskills #projectshowcase #learntocode
