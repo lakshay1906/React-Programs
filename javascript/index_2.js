786f90a2c5c94e37e68a1bcde44b460f0cf88efb
lakshay1906 <lbhati020@gmail.com>
2024-05-26 16:50:46 +0530

    JS Practice

--- javascript/index_2.js
+++ javascript/index_2.js
@@ -42,16 +42,50 @@
 
 // destructuring
 
-let a = 10;
-let arr = [a, 1, 2, 3, 4, 5];
-let obj = {
-  name: "Lakshay",
-  skill: "React",
-};
+// let a = 10;
+// let arr = [a, 1, 2, 3, 4, 5];
+// let obj = {
+//   name: "Lakshay",
+//   skill: "React",
+// };
 
-let [c, d] = arr;
-console.log(c, d);
-let { name } = obj;
-console.log(obj["skill"]);
+// let [c, d] = arr;
+// console.log(c, d);
+// let { name } = obj;
+// console.log(obj["skill"]);
 
 // if we want to destructure an array then we can set any name as another varibale, but when we destructure an object then we are not creating another variable we have to use the key to take a particular value out of it.
+
+function timeAgo(dateString) {
+  // Parse the given date string into a Date object
+  let givenDate = new Date(dateString);
+
+  // Get the current date and time
+  let currentDate = new Date();
+
+  // Calculate the difference in milliseconds
+  let differenceInMillis = currentDate - givenDate;
+
+  // Convert milliseconds to hours
+  let differenceInHours = differenceInMillis / (1000 * 60 * 60);
+
+  // Determine the appropriate message based on the time difference
+  if (differenceInHours < 1) {
+    return "Less than an hour ago";
+  } else if (differenceInHours >= 1 && differenceInHours < 2) {
+    return "1 hour ago";
+  } else if (differenceInHours >= 2 && differenceInHours < 24) {
+    return `${Math.ceil(differenceInHours)} hours ago`;
+  } else if (differenceInHours >= 24 && differenceInHours < 48) {
+    return "1 day ago";
+  } else if (differenceInHours >= 48 && differenceInHours < 168) {
+    return `${Math.ceil(differenceInHours / 24)} days ago`;
+  } else {
+    return "1 week ago";
+  }
+}
+
+let dateStr = "2024-05-08T04:07:33Z";
+let timeDifferenceMessage = timeAgo(dateStr);
+
+console.log(timeDifferenceMessage);
